8bf072184fe847a7d86240f62e5ef39ac39649c4
IsaacLopezz <islopezgo@est.utn.ac.cr>
2025-07-03 18:03:29 -0600

    Se modifica usuarios

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -4,81 +4,21 @@
  */
 package Usuarios;
 
+import Persons.Person;
 import java.time.LocalDate;
+
+
 
 /**
  *
  * @author Luisk
  */
-public class Usuario {
-    private int id;
-    private String nombre;
-    private LocalDate fechaNacimiento;
-    private String telefono;
-
-    public int getId() {
-        return id;
+public class Usuario extends Person {
+    public Usuario(String id, String name, LocalDate birthDate, String phone) {
+        super(id, name, birthDate, phone);
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-    
-    public boolean validarTelefono(String telefono) {
-        if (telefono == null || telefono.isEmpty()) {
-            return true; 
-        }
-
-        if (telefono.length() != 8) return false;
-
-        char primerDigito = telefono.charAt(0);
-        if (primerDigito != '2' && primerDigito != '6' && primerDigito != '7' && primerDigito != '8') {
-            return false;
-        }
-
-        for (int i = 0; i < telefono.length(); i++) {
-            if (!Character.isDigit(telefono.charAt(i))) return false;
-        }
-
-        return true;
-    }
-    
-    public boolean modificarTelefono(String nuevoTelefono) {
-        if (validarTelefono(nuevoTelefono)) {
-            this.telefono = nuevoTelefono;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public Usuario(int id, String nombre, LocalDate fechaNacimiento, String telefono) {
-        this.id = id;
-        this.nombre = nombre;
-        this.fechaNacimiento = fechaNacimiento;
-        
-        if (validarTelefono(telefono)){
-            this.telefono = telefono;
-        }else{
-            this.telefono = null;
-        }
-    }
-
-    @Override
     public String toString() {
-        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + '}';
+        return "id=" + id + ", name=" + name + ", birthDate=" + birthDate + " +, phone=" + phone;
     }
-   
+    
 }
